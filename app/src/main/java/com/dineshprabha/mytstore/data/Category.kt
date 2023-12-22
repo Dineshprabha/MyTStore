de1dc945ee5a99708e03db19b33f38f4f1180122
Dinesh N <dinesh.prabha1205@gmail.com>
2023-12-22 11:04:29 +0530

    Added functionality to fetch product details from firebase for all categories.

--- app/src/main/java/com/dineshprabha/mytstore/data/Category.kt
+++ app/src/main/java/com/dineshprabha/mytstore/data/Category.kt
@@ -3,7 +3,7 @@ package com.dineshprabha.mytstore.data
 sealed class Category (val category: String){
 
     object Chair : Category("Chair")
-    object Cupboadr : Category("Cupboadr")
+    object Cupboard : Category("Cupboard")
     object Table : Category("Table")
     object Furniture : Category("Furniture")
 }
