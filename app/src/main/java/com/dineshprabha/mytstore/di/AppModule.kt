2002e37f4fa73be5ca3eef8a739b280a1b2909a1
Dinesh N <dinesh.prabha1205@gmail.com>
2023-12-22 15:00:07 +0530

    Implemented add product to cart functionality using collections.

--- app/src/main/java/com/dineshprabha/mytstore/di/AppModule.kt
+++ app/src/main/java/com/dineshprabha/mytstore/di/AppModule.kt
@@ -2,8 +2,10 @@ package com.dineshprabha.mytstore.di
 
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
+import com.dineshprabha.mytstore.firebase.FirebaseCommon
 import com.dineshprabha.mytstore.utils.Constants.INTRODUCTION_SP
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
@@ -28,4 +30,11 @@ object AppModule {
     fun provideIntroductionSP(
         application: Application
     ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun provideFirebaseCommon(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore
+    ) = FirebaseCommon(firestore, firebaseAuth)
 }
---
+++ app/src/main/java/com/dineshprabha/mytstore/firebase/FirebaseCommon.kt
@@ -0,0 +1,39 @@
+package com.dineshprabha.mytstore.firebase
+
+import com.dineshprabha.mytstore.data.Cartproduct
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+
+class FirebaseCommon(
+    private val firestore: FirebaseFirestore,
+    private val auth : FirebaseAuth
+) {
+    private val cartCollection = firestore.collection("user").document(auth.uid!!).collection("cart")
+
+    fun addProductToCart(cartProduct: Cartproduct, onResult: (Cartproduct?, Exception?) -> Unit){
+        cartCollection.document().set(cartProduct)
+            .addOnSuccessListener {
+                onResult(cartProduct, null)
+            }.addOnFailureListener {
+                onResult(null, it)
+            }
+    }
+
+    fun incrementQuantity(documentId: String, onResult: (String?, Exception?) -> Unit) {
+        firestore.runTransaction { transition ->
+            val documentRef = cartCollection.document(documentId)
+            val document = transition.get(documentRef)
+            val productObject = document.toObject(Cartproduct::class.java)
+            productObject?.let { cartProduct ->
+                val newQuantity = cartProduct.quantity + 1
+                val newProductObject = cartProduct.copy(quantity = newQuantity)
+                transition.set(documentRef, newProductObject)
+            }
+        }.addOnSuccessListener {
+            onResult(documentId, null)
+        }.addOnFailureListener {
+            onResult(null, it)
+        }
+    }
+}
